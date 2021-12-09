19781fa0186a46901708395c9d2d5e806ad8391e
ck-chenkang <bettermanchenkang@163.com>
2021-12-09 14:28:28 +0800

    modify style

--- web-vue2/src/components/Layout/Home.vue
+++ web-vue2/src/components/Layout/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="home">
     <el-container>
-      <el-aside class="home-menu" width="200px" >
+      <el-aside class="home-menu" width="200px">
         <left-menu></left-menu>
       </el-aside>
       <el-container>
-        <el-main>
+        <el-main class="main">
           <router-view></router-view>
         </el-main>
       </el-container>
@@ -21,8 +21,22 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.home-menu{
-  height: 906px;
+.home-menu {
   background-color: #545c64;
+
+  // top、left、bottom结合 position: absolute定位，刚好完成盒子的定位
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+}
+
+.main {
+  background-color:#545c64;
+  width: 1670px;
+  position: absolute;
+  right: 20px;
+  top: 20px;
+  bottom: 20px;
 }
 </style>
